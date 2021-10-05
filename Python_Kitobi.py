1786ba22f8dfed24a28007b602b13fe7220ed351
Abbosxon Yuldashev <90797368+abbosxon1993@users.noreply.github.com>
2021-10-05 10:30:17 +0500

    Python uz kitobidan masalalar
    Anvar Narzullayevning kitobidan

---
+++ Python_Kitobi.py
@@ -0,0 +1,6 @@
+# .sort() metodi haqida
+cars = ['bmw', 'volvo', 'gm', 'tesla', 'audi']
+# cars.sort()                                      # Hariflar bo,yicha tartiblash
+# cars.sort(reverse=True)                          # Teskari tartibda tartiblash
+cars.reverse
+print(cars)
